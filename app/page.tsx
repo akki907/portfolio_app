0fa5765ce7ef73a0c87c6f98e20df1add60148c5
Akash kumar <ak.akki907@gmail.com>
2024-04-26 09:43:00 +0530

    ui update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, { Suspense, useState, useEffect } from "react";
+import React, { Suspense, useState } from "react";
 import About from "@/components/about";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
@@ -32,7 +32,6 @@ export default function Home() {
             <Suspense fallback={<Preloader />}>
               <>
                 <div className="bg-[#946263] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
-                <div className="bg-[#676394] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] "></div>
                 <Header />
                 <ProgressIndicator />
                 <Intro />
