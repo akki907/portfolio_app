4f713f3b7230ee1208555dc06c24fa6b58c52931
Akash kumar <ak.akki907@gmail.com>
2024-05-24 18:24:24 +0530

    TextGenerateEffect

--- components/about.tsx
+++ components/about.tsx
@@ -4,6 +4,21 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+
+const words = `
+After graduating with a degree in
+Computer science, I decided to pursue my
+passion for programming. I have been coding for 10 years now.
+My favorite part of programming is the
+problem-solving aspect. I love the
+feeling of finally figuring out a solution to a problem. My core stack
+is
+React, Next.js, Node.js, golang and MongoDB
+I am also familiar with TypeScript and Aws. I am always looking to
+learn new technologies. I am currently looking for a
+full-time position as a software developer
+`
 
 export default function About() {
   const { ref } = useSectionInView("About");
@@ -18,23 +33,9 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <p className="mb-3 font-medium">
-        After graduating with a degree in
-        Computer science, I decided to pursue my
-        passion for programming. I have been coding for 10 years now.
-        My favorite part of programming is the
-        problem-solving aspect. I love the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is
-        React, Next.js, Node.js, golang and MongoDB
-        I am also familiar with TypeScript and Aws. I am always looking to
-        learn new technologies. I am currently looking for a
-        full-time position as a software developer
-      </p>
-      <p className="italic">
-       {`When I'm not coding, I enjoy Traveling
-        watching movies, and learning new things.`}
-      </p>
+      <TextGenerateEffect className="mb-3 font-medium" words={words} />      
+      <TextGenerateEffect words={`When I'm not coding, I enjoy Traveling
+        watching movies, and learning new things.`} className="italic"/>
     </motion.section>
   );
 }
