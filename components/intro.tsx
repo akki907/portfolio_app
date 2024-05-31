78e6cb961bfb30eaa7fc85716f3a1ce91cd320a1
Akash kumar <ak.akki907@gmail.com>
2024-05-31 23:01:23 +0530

    ui style

--- components/intro.tsx
+++ components/intro.tsx
@@ -83,7 +83,7 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group opacity-60  bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group custom-gradiant  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href={contactData.cvLink}
           download
           target="_blank"
