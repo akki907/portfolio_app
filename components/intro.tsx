114d8f0ac6c44974a571968da6021d45e94d3593
Akash kumar <ak.akki907@gmail.com>
2024-05-31 23:06:29 +0530

    color chnage

--- components/intro.tsx
+++ components/intro.tsx
@@ -83,7 +83,7 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group custom-gradiant  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group custom-gradiant text-black  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
           href={contactData.cvLink}
           download
           target="_blank"
