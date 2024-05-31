4e11f509fc9b02d9c2413a272d556e1e90afb9db
Akash kumar <ak.akki907@gmail.com>
2024-05-31 22:08:10 +0530

    gitgub graph refactor

--- components/Github-graph.tsx
+++ components/Github-graph.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 import { Button } from './ui/button';
 import { contactData } from '@/lib/data';
-import { WobbleCard } from './ui/wobble-card';
 
 const currentYear = new Date().getFullYear();
 
@@ -13,43 +12,41 @@ export default function GithubGraphSection() {
     const [year, setYear] = useState(currentYear);
 
     return (
-        <WobbleCard containerClassName="col-span-1 py-10 w-full  sm:px-32 px-0 lg:col-span-3 bg- min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
-            <section >
-                <div className=''>
-                    <div className='font-bold capitalize'>
-                        <h4>My Github Contributions</h4>
+        <section className={`py-10 w-full card max-w-7xl`}>
+            <div className=''>
+                <div className='font-bold capitalize'>
+                    <h4>My Github Contributions</h4>
+                </div>
+
+                <div className="space-y-6 pb-2 pt-100 md:space-y-4">
+
+                    <div className="mt-3 flex flex-wrap gap-3 text-md leading-7">
+                        {contributionYears.map((singleYear) => (
+                            <Button
+                                key={singleYear}
+                                variant={singleYear === year ? 'default':'outline'}
+                                onClick={() => setYear(singleYear)}
+                            >
+                                {singleYear}
+                            </Button>
+                        ))}
                     </div>
 
-                    <div className="space-y-6 pb-2 pt-100 md:space-y-4">
-
-                        <div className="mt-3 flex flex-wrap gap-3 text-md leading-7">
-                            {contributionYears.map((singleYear) => (
-                                <Button
-                                    key={singleYear}
-                                    variant={singleYear === year ? 'default' : 'outline'}
-                                    onClick={() => setYear(singleYear)}
-                                >
-                                    {singleYear}
-                                </Button>
-                            ))}
-                        </div>
-
-                        <div
-                            className={`p-6 w-full flex justify-center  items-center overflow-hidden rounded-md border-2 border-opacity-60 border-gray-700 transition-all hover:border-primary-900`}
-                        >
-                            <GitHubCalendar
-                                key={`${year}-calendar`}
-                                username={contactData.githubUsername}
-                                year={year}
-                                colorScheme="dark"
-                            />
-                        </div>
-
+                    <div
+                        className={`p-6 w-full flex justify-center  items-center overflow-hidden rounded-md border-2 border-opacity-60 border-gray-700 transition-all hover:border-primary-900`}
+                    >
+                        <GitHubCalendar
+                            key={`${year}-calendar`}
+                            username={contactData.githubUsername}
+                            year={year}
+                            colorScheme="dark"
+                        />
                     </div>
 
                 </div>
-            </section>
-        </WobbleCard>
+
+            </div>
+        </section>
     )
         ;
 }
