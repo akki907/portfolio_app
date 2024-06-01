df88ade7a6e4dc8fd74367fad9597372bdbb1d56
Akash kumar <ak.akki907@gmail.com>
2024-06-01 18:21:53 +0530

    ui change

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,7 +63,7 @@ export default function Intro() {
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-col sm:flex-row items-center justify-center px-4 text-lg font-medium gap-4"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -72,7 +72,7 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
@@ -83,12 +83,12 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group custom-gradiant text-black  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
+          className="group custom-gradiant text-black  px-7 py-3 flex items-center gap-2 rounded outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
           href={contactData.cvLink}
           download
           target="_blank"
         >
-          Download CV{" "}
+          Resume
           <HiDownload className="group-hover:translate-y-1 transition" />
         </a>
     
